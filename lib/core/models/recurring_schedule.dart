b42e08d5b2ff6f4fd449fb43d65283b45731491a
Natale Affinita <67345055+Natal0ne@users.noreply.github.com>
2026-07-07 21:56:35 +0200

    Implement Routine and RecurringSchedule models

--- lib/core/models/recurring_schedule.dart
+++ lib/core/models/recurring_schedule.dart
@@ -1,3 +1,39 @@
 class RecurringSchedule {
+  final String id;
+  final String routineId;
+  final int weekday;
+  final String title;
+  final int durationMinutes;
+  final String notes;
 
+  const RecurringSchedule({
+    required this.id,
+    required this.routineId,
+    required this.weekday,
+    required this.title,
+    required this.durationMinutes,
+    this.notes = '',
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'routineId': routineId,
+      'weekday': weekday,
+      'title': title,
+      'durationMinutes': durationMinutes,
+      'notes': notes,
+    };
+  }
+
+  factory RecurringSchedule.fromMap(Map<String, dynamic> map) {
+    return RecurringSchedule(
+      id: map['id'] as String,
+      routineId: map['routineId'] as String,
+      weekday: map['weekday'] as int,
+      title: map['title'] as String,
+      durationMinutes: map['durationMinutes'] as int,
+      notes: (map['notes'] as String?) ?? '',
+    );
+  }
 }
--- lib/core/models/routine.dart
+++ lib/core/models/routine.dart
@@ -1,2 +1,44 @@
+import 'enums.dart';
+import 'routine_exercise.dart';
+
 class Routine {
+  final String id;
+  final String name;
+  final String description;
+  final RoutineGoal goal;
+  final String estimatedDurationMinutes;
+  final List<RoutineExercise> exercises;
+
+  Routine({
+    required this.id,
+    required this.name,
+    required this.description,
+    required this.goal,
+    required this.estimatedDurationMinutes,
+    required this.exercises,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'description': description,
+      'targetGoal': goal.name,
+      'estimatedDurationMinutes': estimatedDurationMinutes,
+    };
+  }
+
+  factory Routine.fromMap(
+    Map<String, dynamic> map,
+    List<RoutineExercise> exercises,
+  ) {
+    return Routine(
+      id: map['id'] as String,
+      name: map['name'] as String,
+      description: map['description'] as String,
+      goal: RoutineGoal.values.byName(map['targetGoal'] as String),
+      estimatedDurationMinutes: map['estimatedDurationMinutes'] as String,
+      exercises: exercises,
+    );
+  }
 }
