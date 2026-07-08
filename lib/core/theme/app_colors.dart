96a3da38ea7509180524ac42c31d8dec05e8e042
GaetanoSir <g.sirignano11@studenti.unisa.it>
2026-07-08 22:24:46 +0200

    implemented the overall estetics of the app

--- lib/core/theme/app_colors.dart
+++ lib/core/theme/app_colors.dart
@@ -1,3 +1,18 @@
-class AppColors {
+import 'package:flutter/material.dart';
 
+class AppColors {
+  AppColors._();
+
+  static const Color backgroundSurface = Color(0xFF121212);
+
+  static const Color surfaceContainer = Color(0xFF1E1E1E);
+
+  static const Color accent = Color(0xFF96E6A1);
+
+  static const Color textPrimary = Colors.white;
+  static const Color textSecondary = Colors.grey;
+
+  static const Color beginner = Colors.greenAccent;
+  static const Color intermediate = Colors.amberAccent;
+  static const Color advanced = Colors.redAccent;
 }
--- lib/core/theme/enum_theme_extensions.dart
+++ lib/core/theme/enum_theme_extensions.dart
@@ -1,19 +1,95 @@
-extension DifficultyExtension on Difficulty {
+import 'package:flutter/material.dart';
+import 'package:stronger/core/theme/app_colors.dart';
+import 'package:stronger/core/models/enums.dart';
 
+extension DifficultyExtension on Difficulty {
+  Color get color {
+    switch (this) {
+      case Difficulty.beginner:
+        return Colors.greenAccent;
+      case Difficulty.intermediate:
+        return Colors.orangeAccent;
+      case Difficulty.advanced:
+        return Colors.redAccent;
+    }
+  }
 }
 
 extension EquipmentExtension on Equipment {
-
+  Color get color {
+    switch (this) {
+      case Equipment.bodyweight:
+        return Colors.cyanAccent;
+      case Equipment.dumbbell:
+        return Colors.purpleAccent;
+      case Equipment.barbell:
+        return Colors.blueAccent;
+      case Equipment.machine:
+        return Colors.amberAccent;
+      case Equipment.cable:
+        return Colors.pinkAccent;
+    }
+  }
 }
 
 extension RoutineGoalExtension on RoutineGoal {
-
+  Color get color {
+    switch (this) {
+      case RoutineGoal.hypertrophy:
+        return Colors.amberAccent;
+      case RoutineGoal.strength:
+        return Colors.redAccent;
+      case RoutineGoal.endurance:
+        return Colors.lightGreenAccent;
+      case RoutineGoal.powerlifting:
+        return Colors.deepPurpleAccent;
+    }
+  }
 }
 
 extension GoalStatusExtension on GoalStatus {
-
+  Color get color {
+    switch (this) {
+      case GoalStatus.active:
+        return AppColors.accent;
+      case GoalStatus.completed:
+        return Colors.lightGreenAccent;
+      case GoalStatus.abandoned:
+        return Colors.grey;
+    }
+  }
 }
 
 extension GoalCategoryExtension on GoalCategory {
+  Color get color {
+    switch (this) {
+      case GoalCategory.strength:
+        return Colors.redAccent;
+      case GoalCategory.cardio:
+        return Colors.cyanAccent;
+      case GoalCategory.bodyweight:
+        return Colors.amberAccent;
+      case GoalStatus.completed:
+        return Colors.lightGreenAccent;
+      case GoalCategory.frequency:
+        return Colors.lightGreenAccent;
+      default:
+        return Colors.purpleAccent;
+    }
+  }
 
+  String get label {
+    switch (this) {
+      case GoalCategory.strength:
+        return 'Strength';
+      case GoalCategory.cardio:
+        return 'Cardio';
+      case GoalCategory.bodyweight:
+        return 'Body Weight';
+      case GoalCategory.frequency:
+        return 'Frequency';
+      case GoalCategory.other:
+        return 'Other';
+    }
+  }
 }
