521cb8fa7c629e43ca107e6b7bd8a273bb2178b4
Rondosvalle <rubenrusso360@proton.me>
2026-07-09 11:13:28 +0200

    Implemented database provider

--- lib/core/database/database_provider.dart
+++ lib/core/database/database_provider.dart
@@ -1,1 +1,6 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sqflite/sqflite.dart';
 
+final databaseProvider = Provider<Database>((ref) {
+  throw UnimplementedError('Il database non è ancora stato inizializzato nel main.dart');
+});
